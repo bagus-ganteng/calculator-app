064fc67e70a05e6d509e8a1403e3e07897142e87
bagus-ganteng <61874925+bagus-ganteng@users.noreply.github.com>
2020-03-13 06:10:15 +0700

    Update Program.cs
    Perbaikann bug pada program kalkulator

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,14 +23,7 @@ namespace Praktikum_1
             Console.WriteLine("4. Pembagian");
             Console.WriteLine();
             Console.WriteLine("Input [1]...[4] : ");
-            int c = Int16.(Console.ReadLine());
-
-
-            Console.Write("Input angka pertama = ");
-            int a = int.Parse(Console.ReadLine());
-
-            Console.Write("Input angka kedua = ");
-            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
 
             Console.WriteLine();
             if (c < 1)
@@ -43,20 +36,36 @@ namespace Praktikum_1
             }
             else
             {
-                if (c = 1)
+                if (c == 1)
                 {
+                    Console.Write("Input angka pertama = ");
+                    int a = int.Parse(Console.ReadLine());
+                    Console.Write("Input angka kedua = ");
+                    int b = int.Parse(Console.ReadLine());
                     Console.WriteLine("Hasil {0} + {1} = {2} ", a, b, Penjumlahan(a, b));
                 }
-                else if (c = 2)
+                else if (c == 2)
                 {
+                    Console.Write("Input angka pertama = ");
+                    int a = int.Parse(Console.ReadLine());
+                    Console.Write("Input angka kedua = ");
+                    int b = int.Parse(Console.ReadLine());
                     Console.WriteLine("Hasil {0} - {1} = {2} ", a, b, Pengurangan(a, b));
                 }
-                else if (c = 3)
+                else if (c == 3)
                 {
+                    Console.Write("Input angka pertama = ");
+                    int a = int.Parse(Console.ReadLine());
+                    Console.Write("Input angka kedua = ");
+                    int b = int.Parse(Console.ReadLine());
                     Console.WriteLine("Hasil {0} * {1} = {2} ", a, b, Perkalian(a, b));
                 }
-                else if (c = 4)
+                else if (c == 4)
                 {
+                    Console.Write("Input angka pertama = ");
+                    int a = int.Parse(Console.ReadLine());
+                    Console.Write("Input angka kedua = ");
+                    int b = int.Parse(Console.ReadLine());
                     Console.WriteLine("Hasil {0} / {1} = {2} ", a, b, Pembagian(a, b));
                 }
             }
